497ee781f73bb4b2a65ac35311c317a357f6305a
JAY_TECH <jaydendev13@gmail.com>
2025-04-01 14:09:57 +0300

    Update set.js

--- set.js
+++ set.js
@@ -1,12 +1,12 @@
 global.prefix = '/'; // Set Your Preferred Prefix
-global.botName = 'GIFTED-MD'; // Bot Name
+global.botName = 'Dark-MD'; // Bot Name
 global.botVersion = '2.0.0';
-global.botPic = 'https://files.giftedtech.web.id/file/gifted-md.jpg'; // Can use path, can also use link
-global.botToken = '7267479440:AAE1e8a9BEQjAA0LIK8TEb0AS6EaueOUiNE'; // Replace With Your Bot Token (Get it from BotFather)
+global.botPic = 'https://i.ibb.co/7PqQV1p/images-34.jpg'; // Can use path, can also use link
+global.botToken = '7935567739:AAH2fNdNVuX5nRJXwPL1zuFUz4vAX3QUvnM'; // Replace With Your Bot Token (Get it from BotFather)
 global.ownerUsername = 'mouricedevs'; // Replace With Your Telegram Username(For Bot Owner)
 global.timeZone = 'Africa/Nairobi'; // Your Time Zone
 global.ownerId = [5153324742]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
-global.footer = '*Powered by GiftedTech*';
+global.footer = '*Powered by DARK TECH*';
 global.ytdlWeb = 'https://youtube.giftedtech.web.id';
 global.ytdlApi = 'https://giftedtech-ytdl-api.hf.space';
 global.giftedApi = 'https://apis.giftedtech.web.id/api'; // Gifted Api
@@ -14,9 +14,9 @@ global.giftedKey = 'gifted'; // Gifted Api Key
 global.maherApi = 'https://api.nexoracle.com'; // NexOracle Api
 global.maherKey = ''; // NexOracle Api Key
 global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
-global.giftedWaChannel = 'https://whatsapp.com/channel/0029Vb3hlgX5kg7G0nFggl0Y';
-global.giftedRepo = "https://github.com/mauricegift/telegram-bot";
-global.giftedApiRepo = "https://api.github.com/repos/mauricegift/telegram-bot";
+global.giftedWaChannel = 'https://whatsapp.com/channel/0029VarDt9t30LKL1SoYXy26';
+global.giftedRepo = "https://github.com/Jaydendev112/telegram-bot";
+global.giftedApiRepo = "https://api.github.com/repos/Jaydendev112/telegram-bot";
 
 global.giftechMess = {
     internalerr: 'Internal Server Error!',
