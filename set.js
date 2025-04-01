63ac3d480957665b318295dc1ee3c1abcae6e545
JAY_TECH <jaydendev13@gmail.com>
2025-04-01 14:35:41 +0300

    Update set.js

--- set.js
+++ set.js
@@ -3,11 +3,11 @@ global.botName = 'Dark-MD'; // Bot Name
 global.botVersion = '2.0.0';
 global.botPic = 'https://i.ibb.co/7PqQV1p/images-34.jpg'; // Can use path, can also use link
 global.botToken = '7935567739:AAH2fNdNVuX5nRJXwPL1zuFUz4vAX3QUvnM'; // Replace With Your Bot Token (Get it from BotFather)
-global.ownerUsername = 'mouricedevs'; // Replace With Your Telegram Username(For Bot Owner)
+global.ownerUsername = 'Web_x_pro'; // Replace With Your Telegram Username(For Bot Owner)
 global.timeZone = 'Africa/Nairobi'; // Your Time Zone
 global.ownerId = [5153324742]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
 global.footer = '*Powered by DARK TECH*';
-global.ytdlWeb = 'https://youtube.giftedtech.web.id';
+global.ytdlWeb = 'https://youtube.com/@javiel_dev?si=ToFC1idxek0hmzy3';
 global.ytdlApi = 'https://giftedtech-ytdl-api.hf.space';
 global.giftedApi = 'https://apis.giftedtech.web.id/api'; // Gifted Api
 global.giftedKey = 'gifted'; // Gifted Api Key
